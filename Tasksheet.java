265582a82be7ff22b54be8b309b435e09b69828a
RiaAMHerrera <167742845+RiaAMHerrera@users.noreply.github.com>
2024-06-14 14:44:47 +0800

    initial commit

---
+++ Tasksheet.java
@@ -0,0 +1,11 @@
+public class Tasksheet {
+
+    public static void main(String[] args) {
+        //Copied from https://github.com/marcoyimyaem/javaavtivities/blob/main/HelloWorld.java
+        //and modified to successfully execute
+        System.out.println("Hello World!");
+
+        //modified again to display what is needed
+        System.out.println("Kamusta Mundo!");
+    }
+}
